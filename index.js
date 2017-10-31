bb74b3582d828dce72c4ac0ca3c325af35e206f9
boybutcher <BYRONdeLARGE@yahoo.com>
2017-10-31 12:21:17 -0700

    basic calculator appearance

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log('connected index.js')
+const body = document.body;
+const calculation = 0;
+var currentCalculation = null;
+
+const calculator = document.createElement('div');
+
+// calculator display
+const calculatorDisplay = document.createElement('div');
+calculatorDisplay.innerHTML = calculation;
+
+calculator.appendChild(calculatorDisplay);
+
+// buttons container
+const inputs = document.createElement('div');
+
+// creates number buttons
+const createNumberButton = digit => {
+  const button = document.createElement('button');
+  button.innerHTML = digit;
+  button.onclick = () => {
+    console.log(button.innerHTML);
+    console.log(typeof button.innerHTML)
+    console.log(calculatorDisplay.innerHTML);
+    console.log(typeof calculatorDisplay.innerHTML);
+  }
+  return button;
+}
+
+for (var i = 0; i < 10; i++) {
+  inputs.appendChild(createNumberButton(i));
+}
+
+// plus button
+const plusButton = document.createElement('button');
+plusButton.innerHTML = '+';
+plusButton.onclick = () => {
+  currentCalculation = plusButton.innerHTML;
+};
+
+inputs.appendChild(plusButton);
+
+
+calculator.appendChild(inputs);
+
+body.appendChild(calculator);
